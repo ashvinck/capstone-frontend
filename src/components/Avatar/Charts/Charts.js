4908be8479f513cbb562869b487f34b9ad73d2bc
Ashvin C K <ashvinck10@gmail.com>
2022-11-18 14:49:35 +0530

    chart

--- src/components/Avatar/Charts/Charts.js
+++ src/components/Avatar/Charts/Charts.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
+import { CategoryScale, Chart } from "chart.js";
+Chart.register(CategoryScale);
 
 const GrossRevenueChart = () => {
     // X Axis elements
